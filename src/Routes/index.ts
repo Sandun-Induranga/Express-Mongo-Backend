18194e9276f6d22e6bc5c1609a616f7dfe550e25
Sandun Induranga <sandutharu40@gmail.com>
2023-04-22 15:20:39 +0530

    Content Updated

---
+++ src/Routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import UserRoutes from "./UserRoutes";
+import PostRoutes from "./PostRoutes";
+
+const router: Router = Router();
+
+router.use("/api/v1/user", new UserRoutes().getRouter());
+router.use("/api/v1/post", new PostRoutes().getRouter());
