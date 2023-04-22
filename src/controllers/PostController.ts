b6758ee6cdf662b985983778a5754e7a94f648e1
Sandun Induranga <sandutharu40@gmail.com>
2023-04-22 15:05:24 +0530

    Content Updated

---
+++ src/controllers/PostController.ts
@@ -0,0 +1,34 @@
+import { Request, RequestHandler, Response } from "express";
+
+export default class PostController {
+  createPost: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    return res;
+  };
+
+  retriveAllPost: RequestHandler = async (
+    req: Request,
+    res,
+    Response
+  ): Promise<Response> => {
+    return res;
+  };
+
+  updatePost: RequestHandler = async (
+    req: Request,
+    res,
+    Response
+  ): Promise<Response> => {
+    return res;
+  };
+
+  deletePost: RequestHandler = async (
+    req: Request,
+    res,
+    Response
+  ): Promise<Response> => {
+    return res;
+  };
+}
