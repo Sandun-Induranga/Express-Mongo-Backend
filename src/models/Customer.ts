a227f19d51ae29637c6b702e97314f34999b0e36
Sandun Induranga <sandutharu40@gmail.com>
2023-04-28 20:20:34 +0530

    Content Updated

---
+++ src/models/Customer.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+interface ICustomer extends Document {
+  id: string;
+  name: string;
+  address: string;
+  salary: number;
+}
+
+const CustomerSchema = new Schema({
+  id: {
+    type: String,
+    reruired: true,
+  },
+  name: {
+    type: String,
+    reruired: true,
+  },
+  address: {
+    type: String,
+    reruired: true,
+  },
+  salary: {
+    type: Number,
+    reruired: true,
+  },
+});
+
+export const customer = model<ICustomer>("Customer", CustomerSchema);
---
+++ src/models/Item.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+interface ICustomer extends Document {
+  code: string;
+  description: string;
+  qty: string;
+  price: number;
+}
+
+const ItemSchema = new Schema({
+  code: {
+    type: String,
+    reruired: true,
+  },
+  description: {
+    type: String,
+    reruired: true,
+  },
+  qty: {
+    type: String,
+    reruired: true,
+  },
+  price: {
+    type: Number,
+    reruired: true,
+  },
+});
+
+export const item = model("Item", ItemSchema);
